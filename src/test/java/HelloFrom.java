e934a6cb922a13d39aa4652cfe1ee5b8ef6cf3c5
ElenaLukacheva <elenaly4@gmail.com>
2024-04-06 17:06:42 +0300

    HelloFrom

---
+++ src/test/java/HelloFrom.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class HelloFrom {
+
+    @Test
+    public void testHelloFrom(){
+        System.out.println("Hello from Елена Лукачева");
+    }
+
+}
+
+
+
+
